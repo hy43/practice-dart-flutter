09be646572d2095606b07c5c6fbb36bd9738a8f5
hy43 <hyde0401@gmail.com>
2024-02-01 09:27:50 +0900

    add : variables

---
+++ dart-basic/basic.dart
@@ -0,0 +1,10 @@
+// https://dart.dev/language
+
+void main() {
+  var stockName = "Tesla";
+  var startPrice = 1000000;
+  var currentPrice = 1500000;
+  var interval = currentPrice - startPrice;
+
+  print(interval);
+}
